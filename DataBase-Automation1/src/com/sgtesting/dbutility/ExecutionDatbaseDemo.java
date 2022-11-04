5b3c7728585e17f3e8daefbd8ca5aa011707102a
nikhilgowdakc1999 <nikhilgowdakc1999@gmail.com>
2022-11-04 22:35:54 +0530

    DBAuto

---
+++ DataBase-Automation1/src/com/sgtesting/dbutility/ExecutionDatbaseDemo.java
@@ -0,0 +1,17 @@
+package com.sgtesting.dbutility;
+
+public class ExecutionDatbaseDemo {
+
+	public static void main(String[] args) {
+		
+		int rc=DatabaseUtil.getRecords("select * from Employee");
+		System.out.println("# of Records :"+rc);
+
+		int cc=DatabaseUtil.getColumnsCount("select * from Employee");
+		System.out.println("# of Columns :"+cc);
+		
+		String val=DatabaseUtil.getColumnData("select * from Employee where EMPID=02", "EMPNAME");
+		System.out.println("Column Data :"+val);
+	}
+
+}
